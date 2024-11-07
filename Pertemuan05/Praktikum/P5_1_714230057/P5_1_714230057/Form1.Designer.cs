bbae1261710b887023b49cacad8d74ecf3c75a84
hilmibotak <hilmiromadoni@gmail.com>
2024-11-08 00:54:21 +0700

    pertemuan5

---
+++ Pertemuan05/Praktikum/P5_1_714230057/P5_1_714230057/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace P5_1_714230057
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.Hellobutton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLabel.Location = new System.Drawing.Point(5, 20);
+            this.OutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(264, 23);
+            this.OutputLabel.TabIndex = 0;
+            this.OutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Hellobutton
+            // 
+            this.Hellobutton.Location = new System.Drawing.Point(12, 87);
+            this.Hellobutton.Name = "Hellobutton";
+            this.Hellobutton.Size = new System.Drawing.Size(108, 30);
+            this.Hellobutton.TabIndex = 1;
+            this.Hellobutton.Text = "say hello";
+            this.Hellobutton.UseVisualStyleBackColor = true;
+            this.Hellobutton.Click += new System.EventHandler(this.Hellobutton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(200, 87);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(95, 30);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "E&xit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(324, 162);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.Hellobutton);
+            this.Controls.Add(this.OutputLabel);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(5, 20);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "hello word";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button Hellobutton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Pertemuan05/Praktikum/P5_1_714230057/P5_1_714230057/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_1_714230057
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Hellobutton_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "Hello Word";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
